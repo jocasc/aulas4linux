66c239235dbe7e5e073e6efb50ed175d7ebfc109
Joca Cabral <jocascabral@icloud.com>
2018-02-27 20:29:33 -0300

    relacionais

---
+++ aula2/operadores_relacionais.php
@@ -0,0 +1,21 @@
+<?php
+
+echo"<pre>";
+
+//igual
+
+var_dump(2 == 2);
+var_dump(2 == "2");
+
+
+//identico
+var_dump(2 === 2);
+var_dump(2 === "2");
+
+//menor que
+
+var_dump(2 < 5);
+echo"<hr>";
+
+
+?>
