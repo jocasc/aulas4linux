5d212a291d89de013394f45965c9bd2ca0b647cd
Joca Cabral <jocascabral@icloud.com>
2018-02-27 22:19:55 -0300

    case

---
+++ aula2/decisao.php
@@ -0,0 +1,16 @@
+<?php
+
+$nome = readline("Digite o seu nome: ");
+
+switch ($nome) {
+	case 'Diego':
+		echo"Fala aí Diego, blz?";
+		break;
+	
+	default:
+		echo"Não te conheço estranho!";
+		break;
+}
+
+
+?>
